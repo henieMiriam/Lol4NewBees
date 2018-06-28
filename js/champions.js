4566de0b380bd1bb1c01d1d699ead82a7a35aa76
SarahWinchester <indira_ruli@hotmail.com>
2018-06-28 00:29:26 -0500

    algo

---
+++ js/champions.js
@@ -0,0 +1,13 @@
+
+
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDMO_Lp1WJu123MjGNjwuNcuulXCj40eqw",
+  authDomain: "lol-4new-bees.firebaseapp.com",
+  databaseURL: "https://lol-4new-bees.firebaseio.com",
+  projectId: "lol-4new-bees",
+  storageBucket: "lol-4new-bees.appspot.com",
+  messagingSenderId: "898255528761"
+};
+firebase.initializeApp(config);
